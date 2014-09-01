315f4779a0be92fa27bc9ccdbb051bbc6546d311
Mathias Bynens <mathias@qiwi.be>
2014-09-01 11:40:38 +0200

    URLencode the target URL properly

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
-        chrome.tabs.create( { "url": "https://developers.google.com/speed/pagespeed/insights/?url=" +tab[0].url } );
+    chrome.tabs.query({
+        "currentWindow": true,
+        "active": true
+    }, function(tab) {
+        chrome.tabs.create({
+            "url": "https://developers.google.com/speed/pagespeed/insights/?url=" + encodeURIComponent(tab[0].url)
+        });
     });
 });
